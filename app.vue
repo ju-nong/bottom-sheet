193da1dd4f492a4c66dfe5a1edca39060231597a
이준용 <iiiuooyoo@gmail.com>
2023-07-05 11:13:58 +0900

    feat : Meta 추가

--- app.vue
+++ app.vue
@@ -1,4 +1,40 @@
 <template>
+    <Head>
+        <Title>Bottom Sheet</Title>
+        <Meta name="theme-color" content="#e4fff3" />
+        <Meta
+            name="description"
+            content="이상한 botom-sheet 라이브러리 쓰다가, 그냥 내가 만드는게 낫겠다 싶어서 만든 프로젝트"
+        />
+        <Meta name="author" content="이준용" />
+        <Meta
+            name="keywords"
+            content="이준용, bottom, sheet, component, vue, nuxt"
+        />
+        <Meta property="og:type" content="website" />
+        <Meta property="og:title" content="Bottom Sheet" />
+        <Meta
+            property="og:description"
+            content="이상한 botom-sheet 라이브러리 쓰다가, 그냥 내가 만드는게 낫겠다 싶어서 만든 프로젝트"
+        />
+        <Meta property="og:locale" content="ko_KR" />
+        <Meta
+            property="og:url"
+            content="https://open-bottom-sheet.vercel.app/"
+        />
+        <Meta property="og:image" content="/thumbnail.png" />
+        <Meta property="og:site_name" content="생일선물 뽑기" />
+
+        <Meta name="twitter:card" content="summary_large_image" />
+        <Meta name="twitter:title" content="Bottom Sheet" />
+        <Meta
+            name="twitter:description"
+            content="이상한 botom-sheet 라이브러리 쓰다가, 그냥 내가 만드는게 낫겠다 싶어서 만든 프로젝트"
+        />
+        <Meta name="twitter:image" content="/thumbnail.png" />
+
+        <Link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+    </Head>
     <button
         type="button"
         class="open-btn text-white text-[2rem] bg-[#44bd87] px-[30px] py-[6px] rounded-[4px]"
