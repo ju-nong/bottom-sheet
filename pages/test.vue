e6a25b736723f71fb9e74fbe56c81f328e595161
이준용 <iiiuooyoo@gmail.com>
2023-08-01 12:28:09 +0900

    test3

---
+++ pages/test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>안녕하쇼</div>
+
+    <iframe
+        src="https://open-bottom-sheet.vercel.app/"
+        width="300px"
+        height="200px"
+    >
+        <p>사용 중인 브라우저는 iframe을 지원하지 않습니다.</p>
+    </iframe>
+</template>
+
+<script setup></script>
+
+<style lang="scss"></style>
