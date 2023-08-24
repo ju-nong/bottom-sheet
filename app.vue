9b17e3ea0aa1740341e32ceaaa005ce0dd09f379
이준용 <iiiuooyoo@gmail.com>
2023-08-24 13:01:34 +0900

    Revert "test3"
    This reverts commit e6a25b736723f71fb9e74fbe56c81f328e595161.

--- app.vue
+++ app.vue
@@ -35,12 +35,44 @@
 
         <Link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
     </Head>
+    <button
+        type="button"
+        class="open-btn text-white text-[2rem] bg-[#44bd87] px-[30px] py-[6px] rounded-[4px]"
+        @click="show = true"
+    >
+        Open
+    </button>
+    <BottomSheet :show="show" @close="show = false">
+        <div class="flex pt-3 pb-9 items-center justify-center text-center">
+            아ㅋㅋ 겁나 잘 된다~
 
-    <NuxtPage />
+            <br />
+            <br />
+
+            라이브러리 왜 씀?
+        </div>
+    </BottomSheet>
+
+    <!-- <div class="full-modal"></div> -->
 </template>
 
 <script setup>
 const show = ref(false);
+
+onMounted(() => {
+    const $div = document.createElement("div");
+    $div.classList.add("full-modal");
+
+    window.parent.document.body.appendChild($div);
+
+    setTimeout(() => {
+        console.log("콘솔");
+    }, 1000);
+
+    setTimeout(() => {
+        alert("알림");
+    }, 2000);
+});
 </script>
 
 <style lang="scss">
